67c03f7283644fc3c91e3a1a07e940deb5c69941
AbdallahMostafa <abdallahmostafa377@gmail.com>
2021-07-21 23:24:42 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {first} from 'rxjs/operators';
+import { from } from 'rxjs';
+import { analyze } from 'eslint-scope';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  mydata: any;
+  value : any;
+  countryFilter :any;
+  stateFilter : any;
+  filterArray : any;
+  constructor(private http: HttpClient) { }
+  selectedItem = "hello";
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.http.get('http://127.0.0.1:8000/all/').pipe(first()).subscribe(res =>{
+      this.mydata = res;
+    });
+  }
+  
+  filter(){
+    if(this.countryFilter) {
+      this.http.get(`http://127.0.0.1:8000/country_filter/${this.countryFilter}`).pipe(first()).subscribe(res =>{
+        this.mydata = res;
+      });
+    }
+    if(this.stateFilter) {      
+      this.http.get(`http://127.0.0.1:8000/state_filter/${this.stateFilter}`).pipe(first()).subscribe(res =>{
+        this.mydata = res;
+        console.log(this.mydata);
+      });
+    }
+    if(this.countryFilter && this.stateFilter) {
+      this.http.get(`http://127.0.0.1:8000/country_state_filter/${this.countryFilter}/${this.stateFilter}`).pipe(first()).subscribe(res =>{
+        this.mydata = res;
+        console.log(this.mydata);
+      });
+    }
+    
+  }
+
+  selectValid(value : any){
+    this.http.get(`http://127.0.0.1:8000/filter/${value}`).pipe(first()).subscribe(res =>{
+      this.mydata = res;
+      console.log(this.mydata);
+    });
+  }
+  onChange(event: any) {
+    console.log(event);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
